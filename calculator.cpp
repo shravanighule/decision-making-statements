06d29f13aee6db92fd4f01db54942084d0ec65f6
shravanighule <shravanighule16@gmail.com>
2024-08-29 06:42:51 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,35 @@
+//shravani ghule
+//23070123056
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int b,c,d,e;
+    float a,f;
+    cout<<"Enter the value of a: "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b: "<<endl;
+    cin>>b;
+    c = a+b;
+    cout<<"Sum is: "<<c<<endl;
+    d = a-b;
+    cout<<"Difference is: "<<d<<endl;
+    e = a*b;
+    cout<<"Produt is: "<<e<<endl;
+    f = a/b;
+    cout<<"Division is: "<<f<<endl;
+}
+
+
+
+/*output
+Enter the value of a:
+55
+Enter the value of b:
+66
+Sum is: 121
+Difference is: -11
+Produt is: 3630
+Division is: 0.833333
+/*
