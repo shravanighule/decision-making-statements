355f070e650de9cd4ccf34efee68c12c1299e5ec
shravanighule <shravanighule16@gmail.com>
2024-08-29 06:50:38 +0530

    Add files via upload

---
+++ greatest no.cpp
@@ -0,0 +1,32 @@
+//shravani ghule
+//23070123056
+//exp5-largest of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter 1st number: ";
+    cin>>a;
+    cout<<"Enter 2nd number: ";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<a<<" is greater than "<<b;
+    
+    }
+    else
+    {
+        cout<<b<<" is greater than "<<a;
+
+    }
+    return 0;
+}
+
+
+
+/*output
+Enter 1st number: 30
+Enter 2nd number: 40
+40 is greater than 30
+/*
