e747780172ef9b821ae84b95ed8ca765f7aa37bd
shravanighule <shravanighule16@gmail.com>
2024-08-29 06:45:35 +0530

    Add files via upload

---
+++ even and odd.cpp
@@ -0,0 +1,23 @@
+//Gayatri Apotikar
+//23070123055
+#include <iostream>
+using namespace std;
+int main()
+ {
+    int n;
+    cout<<"enter a no"<<endl;
+    cin>>n;
+    if (n%2==0)
+    cout<<"it is even no"<<endl;
+    else
+    cout<<"it is odd no"<<endl;
+    return 0;
+}
+
+
+/*output
+enter a no
+40
+it is even no
+/*
+
